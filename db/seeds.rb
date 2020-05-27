6040c33b44ecdc4db0cddbb63e6f58716266be0f
erikalvar <erikalvarbass@gmail.com>
2020-05-27 13:29:58 -0400

    adds products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# product = Product.new(
+#   name: "tomato seeds",
+#   price: "3",
+#   image_url: "https://d1nw62gticy6e9.cloudfront.net/uploads/Sweetie-Cherry-Tomato-Seed.jpg",
+#   description: "50 seeds; heirloom cherry tomatoes."
+# )
+# product.save
+
+# product = Product.new(
+#   name: "cucumber seeds",
+#   price: "4",
+#   image_url: "https://parkseed.com/images/xxl/52425-PK-P1.jpg",
+#   description: "40 seeds."
+# )
+# product.save
+
+# product = Product.new(
+#   name: "red bell pepper seeds",
+#   price: "5",
+#   image_url: "https://i.etsystatic.com/9586350/r/il/797738/1214750908/il_570xN.1214750908_d0jj.jpg",
+#   description: "30 seeds; heirloom."
+# )
+# product.save
