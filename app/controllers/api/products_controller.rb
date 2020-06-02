dca1a38eb97b1b8965e1fabd99f0983e219aab23
erikalvar <erikalvarbass@gmail.com>
2020-06-02 14:08:49 -0400

    adds restfull routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,15 +1,40 @@
 class Api::ProductsController < ApplicationController
 
-  def all_products
-    @all_products = Product.all
-    render "products.json.jb"
+  def index
+    @products = Product.all
+    render "index.json.jb"
   end
 
-  def tomato_seeds
-    @tomato_seeds = Product.find_by(name: "tomato seeds")
-    render "tomato_seeds.json.jb"
+  def show
+    @product = Product.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render "show.json.jb"    
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.name
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "Recipe destoyed."}
   end
 
 end
-
-
